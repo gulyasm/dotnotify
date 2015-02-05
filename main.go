920d8c9c048ba08697ec67e12cc9661964f86692
gulyasm <mgulyas86@gmail.com>
2015-02-05 18:45:02 +0100

    Init commit.

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"github.com/howeyc/fsnotify"
+	"log"
+)
+
+func handle(w *fsnotify.Watcher) {
+	for {
+		select {
+		case ev := <-w.Event:
+			if ev.IsModify() && !ev.IsAttrib() {
+				log.Println(fmt.Sprintf("Changed: %s", ev.Name))
+				upload(ev.Name)
+			}
+		case err := <-w.Error:
+			log.Println(err)
+		}
+	}
+}
+
+func main() {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Panic("Error occured.")
+	}
+
+	done := make(chan bool)
+	go handle(watcher)
+	err = watcher.Watch(".")
+	if err != nil {
+		log.Panic("Panic error")
+	}
+	<-done
+	watcher.Close()
+}
